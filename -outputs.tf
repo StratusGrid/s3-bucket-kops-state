3215dc55e7d1b000a64d28030ae2ba203f8219d5
StratusChris <41697652+StratusChris@users.noreply.github.com>
2018-12-10 22:35:21 -0500

    Update -outputs.tf

--- -outputs.tf
+++ -outputs.tf
@@ -1,3 +1,9 @@
+output "bucket_arn" {
+  description = "ARN of bucket to be referenced in policies etc."
+  value = "${aws_s3_bucket.bucket.id}"
+}
+
 output "bucket_id" {
+  description = "ID of bucket"
   value = "${aws_s3_bucket.bucket.id}"
 }
