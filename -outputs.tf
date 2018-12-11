f914767477fef8effffaad1391eb95d1cc3c6dd4
StratusChris <41697652+StratusChris@users.noreply.github.com>
2018-12-11 01:54:43 -0500

    Update -outputs.tf

--- -outputs.tf
+++ -outputs.tf
@@ -1,23 +1,9 @@
-output "bucket_arn_kms" {
-  description = "ARN of bucket to be referenced in policies etc."
-  count = "${var.sse_kms}"
-  value = "${join(",", aws_s3_bucket.bucket_kms.*.arn)}"
-}
-
-output "bucket_id_kms" {
-  description = "ID of bucket"
-  count = "${var.sse_kms}"
-  value = "${join(",", aws_s3_bucket.bucket_kms.*.id)}"
-}
-
 output "bucket_arn" {
   description = "ARN of bucket to be referenced in policies etc."
-  count = "${1 - var.sse_kms}"
-  value = "${join(",", aws_s3_bucket.bucket.*.arn)}"
+  value = "${join(",", concat(aws_s3_bucket.bucket_kms.*.arn, aws_s3_bucket.bucket.*.arn))}"
 }
 
 output "bucket_id" {
   description = "ID of bucket"
-  count = "${1 - var.sse_kms}"
-  value = "${join(",", aws_s3_bucket.bucket.*.id)}"
+  value = "${join(",", concat(aws_s3_bucket.bucket_kms.*.id, aws_s3_bucket.bucket.*.id))}"
 }
