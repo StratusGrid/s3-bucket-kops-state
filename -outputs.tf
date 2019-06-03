624d3772e8d95ab5061e92d13557278074265de5
Chris Hurst <chris.hurst@stratusgrid.com>
2019-06-03 12:09:37 -0400

    additional outputs

--- -outputs.tf
+++ -outputs.tf
@@ -7,3 +7,11 @@ output "bucket_id" {
   description = "ID of bucket"
   value = "${join(",", concat(aws_s3_bucket.bucket_kms.*.id, aws_s3_bucket.bucket.*.id))}"
 }
+
+output "kms_key_alias_arn" {
+  value = "${aws_kms_alias.key_alias_kms.arn}"
+}
+
+output "kms_key_arn" {
+  value = "${aws_kms_key.key_kms.arn}"
+}
