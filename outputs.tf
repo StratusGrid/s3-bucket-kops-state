6ae51da633a418d071213d7426d00e9d13d98f4d
DJKaswa <dieudonne.kaswa@stratusgrid.com>
2022-10-26 05:40:16 -0500

    Fixes terraform_deprecated_index

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "bucket_arn" {
   description = "ARN of bucket to be referenced in policies etc."
-  value       = join(",", concat(aws_s3_bucket.bucket_kms.*.arn, aws_s3_bucket.bucket.*.arn))
+  value       = join(",", concat(aws_s3_bucket.bucket_kms[0].arn, aws_s3_bucket.bucket[0].arn))
 }
 
 output "bucket_id" {
   description = "ID of bucket"
-  value       = join(",", concat(aws_s3_bucket.bucket_kms.*.id, aws_s3_bucket.bucket.*.id))
+  value       = join(",", concat(aws_s3_bucket.bucket_kms[0].id, aws_s3_bucket.bucket[0].id))
 }
 
 output "kms_key_alias_arn" {
